4e8981e5ddab7cbcb53c227227c844e74e8bfcbf
Anastasia Naumenko <charlieplanka@gmail.com>
2021-02-09 13:43:48 +0300

    Add car model

--- car_filter/admin.py
+++ car_filter/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Car
 
-# Register your models here.
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    @staticmethod
+    def car(self):
+        return self
+
+    list_display = ('car', 'year', 'transmission', 'color')
--- car_filter/models.py
+++ car_filter/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    TRANSMISSION_CHOICES = [
+        (1, "механика"),
+        (2, "автомат"),
+        (3, "робот"),
+    ]
+
+    manufacturer = models.CharField(max_length=200)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    transmission = models.SmallIntegerField(choices=TRANSMISSION_CHOICES)
+    color = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.manufacturer} {self.model}"
+
