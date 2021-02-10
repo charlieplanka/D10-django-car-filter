c7379f48b45b01b8c1dffd81d99966938a33607c
Anastasia Naumenko <charlieplanka@gmail.com>
2021-02-10 14:15:34 +0300

    Improve filter

--- car_filter/views.py
+++ car_filter/views.py
@@ -7,14 +7,11 @@ from django.db.models import Q
 class CarListView(ListView):
     model = Car
 
-    def get_queryset(self):
-        qs = super().get_queryset()
-        get_params = self.request.GET.dict()
-
+    def get_queryset(self):        
         # filter
+        get_params = self.request.GET.dict()
         filter_params = Q()
         for key, value in get_params.items():
             if value:
-                filter_params.add(Q(**{f'{key}__icontains': value}), Q.AND)
-        qs = qs.filter(filter_params)
-        return qs
+                filter_params.add(Q(**{f'{key}__icontains': value.strip()}), Q.AND)
+        return Car.objects.filter(filter_params)
