c9a48fd687cdccc9c8071b66b588db52a6898af6
Anastasia Naumenko <charlieplanka@gmail.com>
2021-02-10 13:41:56 +0300

    Add filter

--- car_filter/views.py
+++ car_filter/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from .models import Car
 from django.views.generic import ListView
+from django.db.models import Q
 
 
 class CarListView(ListView):
     model = Car
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        get_params = self.request.GET.dict()
+
+        # filter
+        filter_params = Q()
+        for key, value in get_params.items():
+            if value:
+                filter_params.add(Q(**{key: value}), Q.AND)
+        qs = qs.filter(filter_params)
+        return qs
